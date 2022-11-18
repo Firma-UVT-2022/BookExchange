bfbf053ec05667d6fcb3bfef7c3ca59b1a8c55ca
sebibitica <93866140+sebibitica@users.noreply.github.com>
2022-11-18 03:10:54 +0200

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 const HomeScreen = () => {
     return (
         <View>
-            <Text>Home</Text>
+            <Text>Homeee</Text>
         </View>
     )
 }
