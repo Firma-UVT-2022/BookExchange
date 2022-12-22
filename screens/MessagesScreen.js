654276f485019bed4b5aadbbb4c0d413ec4a1d4f
Sebastian Bitica <biticasebi@gmail.com>
2022-12-22 17:46:07 +0200

    pagina conversatiilor

--- screens/MessagesScreen.js
+++ screens/MessagesScreen.js
@@ -1,117 +1,184 @@
-import { setStatusBarNetworkActivityIndicatorVisible } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, TextInput, Button, FlatList, TouchableOpacity, Image, RefreshControl } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  RefreshControl,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
 
-import { firestore, auth } from '../firebase';
+import { firestore, auth } from "../firebase";
 
 export default function MessagesPage({ navigation }) {
-    const [user, setUser] = useState('');
-    const [users, setUsers] = useState(null);
-    const [refreshing, setRefreshing] = useState(false);
-    const [daca, setDaca] = useState(false);
+  const [user, setUser] = useState("");
+  const [users, setUsers] = useState(null);
+  const [refreshing, setRefreshing] = useState(false);
+  const [daca, setDaca] = useState(false);
 
-    useEffect(() => {
-        firestore.collection("users")
-            .doc(auth.currentUser.uid).get()
-            .then(snapshot => {
-                if (snapshot.exists) {
-                    setUser(snapshot.data())
-                }
-            })
-    }, [])
+  useEffect(() => {
+    firestore
+      .collection("users")
+      .doc(auth.currentUser.uid)
+      .get()
+      .then((snapshot) => {
+        if (snapshot.exists) {
+          setUser(snapshot.data());
+        }
+      });
+  }, []);
 
-    const getUsers = async () => {
-        if (!user) return;
-        const chatQuerySnap = await firestore.collection("Chats")
+  const getUsers = async () => {
+    if (!user) return;
+    const chatQuerySnap = await firestore.collection("Chats").get();
+    const chatIds = chatQuerySnap.docs.map((doc) => doc.id);
+    const idsarray = [];
+    const arrayids = [];
+    for (let i in chatIds) {
+      idsarray.push(chatIds[i]);
+      let temporar = chatIds[i].split("-");
+      if (
+        temporar[0] === auth.currentUser.uid ||
+        temporar[1] === auth.currentUser.uid
+      ) {
+        arrayids.push(temporar[0]);
+        arrayids.push(temporar[1]);
+      }
+    }
+    if (arrayids.length > 0) {
+      const querySnap = await firestore
+        .collection("users")
+        .where("userId", "in", arrayids)
+        .where("userId", "!=", auth.currentUser.uid)
+        .get();
+
+      const allUsers = querySnap.docs.map((doc) => doc.data());
+      const sortedUsers = await Promise.all(
+        allUsers.map(async (a) => {
+          const chatida =
+            a.userId > auth.currentUser.uid
+              ? auth.currentUser.uid + "-" + a.userId
+              : a.userId + "-" + auth.currentUser.uid;
+          const cola = await firestore.collection("Chats").doc(chatida).get();
+          const locatieUser = await firestore
+            .collection("users")
+            .doc(cola.data().whosent)
             .get();
-        const chatIds = chatQuerySnap.docs.map(doc => doc.id);
-        const idsarray = [];
-        const arrayids = [];
-        for (let i in chatIds) {
-            idsarray.push(chatIds[i]);
-            let temporar = chatIds[i].split("-");
-            if (temporar[0] === auth.currentUser.uid ||
-                temporar[1] === auth.currentUser.uid) {
-                arrayids.push(temporar[0]);
-                arrayids.push(temporar[1]);
-            }
-        }
-        if (arrayids.length > 0) {
-            const querySnap = await firestore.collection('users')
-                .where("userId", "in", arrayids)
-                .where("userId", "!=", auth.currentUser.uid)
-                .get()
+          const numeleUserului = locatieUser.data().firstname;
+          return {
+            ...a,
+            da: cola.data().da,
+            ultimMesaj: cola.data().msg,
+            deLa: numeleUserului,
+          };
+        })
+      ).then((allUsers) =>
+        allUsers.sort((a, b) => b.da.seconds - a.da.seconds)
+      );
 
-            const allUsers = querySnap.docs.map(doc => doc.data())
-            const sortedUsers = await Promise.all(allUsers.map(async (a) => {
-                const chatida = a.userId > auth.currentUser.uid ? auth.currentUser.uid + "-" + a.userId : a.userId + "-" + auth.currentUser.uid;
-                const cola = await firestore.collection('Chats').doc(chatida).get();
-                return { ...a, da: cola.data().da };
-            })).then((allUsers) => allUsers.sort((a, b) => b.da.seconds - a.da.seconds))
-
-            setUsers(sortedUsers);
-        }
-        else {
-            setUsers(null);
-        }
-        if (users === null)
-            setDaca(true);
-
+      setUsers(sortedUsers);
+    } else {
+      setUsers(null);
     }
-    useEffect(() => {
-        getUsers()
-        setRefreshing(false);
-    }, [user, refreshing, daca])
+  };
+  useEffect(() => {
+    getUsers();
+    setRefreshing(false);
+  }, [user, users, refreshing]);
+  //console.log(users);
+  const Refresher = () => {
+    setRefreshing(true);
+  };
 
-
-    const Refresher = () => {
-        setRefreshing(true);
-    }
-
-    //const chatid = users > currUserId ? currUserId + "-" + destUserId : destUserId + "-" + currUserId
-    return (
-        <View style={styles.container}>
-            <FlatList
-                data={users}
-                keyExtractor={(item) => item.userId}
-                refreshControl={
-                    <RefreshControl refreshing={refreshing} onRefresh={Refresher} />
-                }
-                renderItem={({ item }) => (
-                    <TouchableOpacity onPress={() => navigation.navigate("Chat", { id: item.userId })}>
-                        <View style={styles.card} >
-                            <Image style={{ width: 30, height: 30 }} source={{ uri: item.pfp }} />
-                            <View style={styles.textArea}>
-                                <Text style={{ fontSize: 15, fontWeight: "bold" }} >{item.firstname + " " + item.lastname}</Text>
-                            </View>
-                        </View>
-                    </TouchableOpacity>
-                )}
-            />
-        </View>
-    );
+  //const chatid = users > currUserId ? currUserId + "-" + destUserId : destUserId + "-" + currUserId
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={{ fontSize: 25, fontWeight: "bold" }}>Messages</Text>
+      </View>
+      <FlatList
+        data={users}
+        keyExtractor={(item) => item.userId}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={Refresher} />
+        }
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("Chat", { id: item.userId })}
+          >
+            <View style={styles.card}>
+              <View style={styles.imageArea}>
+                <Image
+                  style={{
+                    width: 60,
+                    height: 60,
+                    borderRadius: 30,
+                    borderColor: "black",
+                    borderWidth: 1,
+                  }}
+                  source={{ uri: item.pfp }}
+                />
+              </View>
+              <View style={styles.textArea}>
+                <Text style={{ fontSize: 15, fontWeight: "bold" }}>
+                  {item.firstname + " " + item.lastname}
+                </Text>
+                <Text style={{ fontSize: 15, fontWeight: "400" }}>
+                  {item.deLa + ": " + item.ultimMesaj}
+                </Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+    </SafeAreaView>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-        alignItems: "center",
-        flex: 1,
-        paddingTop: 30,
-    },
-    card: {
-        marginTop: 10,
-        width: "95%",
-        height: 50,
-        flexDirection: "row",
-        backgroundColor: "blue",
-        alignItems: "center",
-        justifyContent: "center",
-        borderRadius: 5,
-    },
-    textArea: {
-        width: "60%",
-        alignItems: "center",
-        justifyContent: "center"
-    }
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+  },
+  header: {
+    width: "100%",
+    backgroundColor: "#2490ef",
+    padding: 15,
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomLeftRadius: 15,
+    borderBottomRightRadius: 15,
+    elevation: 20,
+  },
+  card: {
+    marginTop: 10,
+    width: "100%",
+    height: 80,
+    flexDirection: "row",
+    //backgroundColor: "blue",
+    //alignItems: "center",
+    //justifyContent: "center",
+    borderRadius: 5,
+  },
+  textArea: {
+    width: "60%",
+    //salignItems: "center",
+    justifyContent: "center",
+  },
+  imageArea: {
+    width: "20%",
+    //backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  ultimaOra: {
+    width: "20%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
